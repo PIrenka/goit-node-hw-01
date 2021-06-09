a1ad3e6778ec34f4ba34ab6027a777ffe47ae7a8
Iryna Protsenko <irene.protsenko@gmail.com>
2021-06-10 01:08:12 +0300

    step 2 and 3 done

---
+++ index.js
@@ -0,0 +1,12 @@
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./contacts.js");
+
+listContacts();
+// addContact("david", "email@gmail.com", "12345678");
+// addContact("david", "email555@gmail.com", "12345678");
+// removeContact(1623275852074);
+// listContacts();
